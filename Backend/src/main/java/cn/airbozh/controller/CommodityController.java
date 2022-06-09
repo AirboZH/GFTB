14113b3de007774a2470206834bb059575c6941f
AirboZH <airbozh@qq.com>
2022-06-10 03:29:21 +0800

    chore：细节变动

--- Backend/src/main/java/cn/airbozh/controller/CommodityController.java
+++ Backend/src/main/java/cn/airbozh/controller/CommodityController.java
@@ -9,9 +9,13 @@ import cn.airbozh.utility.Auth.AuthException;
 import cn.airbozh.utility.ResMsg;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class CommodityController {
@@ -21,13 +25,13 @@ public class CommodityController {
     UserService userService;
     ResMsg<?> resMsg;
 
-    @RequestMapping(value = "/commodity",method = RequestMethod.POST)
+    @RequestMapping(value = "/commodity", method = RequestMethod.POST)
     @Auth
-    public ResMsg<?> createCommodity(@RequestBody Commodity commodity,User user){
+    public ResMsg<?> createCommodity(@RequestBody Commodity commodity, User user) {
         if (userService.findUserById(user.getUserId()).getRole() < 1)
             throw new AuthException(HttpStatus.UNAUTHORIZED, "没有权限");
-        if(commodityService.createCommodity(commodity))
-            resMsg = new ResMsg<>(1,"Create Success");
+        if (commodityService.createCommodity(commodity))
+            resMsg = new ResMsg<>(1, "Create Success");
         else
             resMsg = new ResMsg<>(0, "Create Fail");
         return resMsg;
@@ -44,8 +48,8 @@ public class CommodityController {
         return resMsg;
     }
 
-    @RequestMapping(value = "/commodity/{commodityId}",method = RequestMethod.GET)
-    public ResMsg<?> getCommodityById(@PathVariable("commodityId") int commodityId){
+    @RequestMapping(value = "/commodity/{commodityId}", method = RequestMethod.GET)
+    public ResMsg<?> getCommodityById(@PathVariable("commodityId") int commodityId) {
         Commodity commodity = commodityService.findCommodityById(commodityId);
         if (commodity == null) {
             resMsg = new ResMsg<>(0, "No Result");
@@ -57,16 +61,16 @@ public class CommodityController {
 
     @RequestMapping(value = "/commodity/{commodityId}", method = RequestMethod.PATCH)
     @Auth
-    public ResMsg<?> updateCommodityById(@PathVariable("commodityId") int commodityId, @RequestBody Commodity commodity,User user) {
+    public ResMsg<?> updateCommodityById(@PathVariable("commodityId") int commodityId, @RequestBody Commodity commodity, User user) {
         if (userService.findUserById(user.getUserId()).getRole() < 1)
             throw new AuthException(HttpStatus.UNAUTHORIZED, "没有权限");
-        Commodity dbCommodity = commodityService.updateCommodity(commodityId,commodity);
-            if (dbCommodity != null)
-                resMsg = new ResMsg<>(1, "Update Success", dbCommodity);
-            else {
-                resMsg = new ResMsg<>(0, "Update Fail");
-            }
-            return resMsg;
+        Commodity dbCommodity = commodityService.updateCommodity(commodityId, commodity);
+        if (dbCommodity != null)
+            resMsg = new ResMsg<>(1, "Update Success", dbCommodity);
+        else {
+            resMsg = new ResMsg<>(0, "Update Fail");
+        }
+        return resMsg;
     }
 
     @RequestMapping(value = "/commodity/{commodityId}", method = RequestMethod.DELETE)
@@ -77,10 +81,42 @@ public class CommodityController {
         if (commodityService.deleteCommodity(commodityId)) {
             resMsg = new ResMsg<>(1, "DeleteCommodity Success");
         } else {
-            resMsg = new ResMsg<>(0, "DeleteCommodityFail");
+            resMsg = new ResMsg<>(0, "DeleteCommodity Fail");
         }
         return resMsg;
     }
 
+    @RequestMapping(value = "/commodity/upload", method = RequestMethod.POST)
+    public ResMsg<?> upload(@RequestParam("file") MultipartFile uploadFile, HttpServletRequest request) {
+        try {
+            // 判断文件是否成功上传
+            if (uploadFile != null) {
+                // 指定上传图片的保存路径
+//                String path = request.getServletContext().getRealPath("/image/");
+                String path = "C:\\Users\\airbo\\Documents\\Code\\GFTB\\backend-frontend\\public\\image\\";
+                // 获取上传的文件名全称
+                String fileName = uploadFile.getOriginalFilename();
+                // 获取上传文件的后缀名
+                String suffix = fileName.substring(fileName.lastIndexOf("."));
+                // 给文件定义一个新的名称,杜绝文件重名覆盖现象
+                String newFileName = UUID.randomUUID().toString() + suffix;
+
+                // 创建File对象,注意这里不是创建一个目录或一个文件,你可以理解为是 获取指定目录中文件的管理权限(增改删除判断等 . . .)
+                File tempFile = new File(path);
+                // 判断File对象对应的目录是否存在
+                if (!tempFile.exists()) {
+                    // 创建以此抽象路径名命名的目录,注意mkdir()只能创建一级目录,所以它的父级目录必须存在
+                    tempFile.mkdir();
+                }
+                // 在指定路径中创建一个文件(该文件是空的)
+                uploadFile.transferTo(new File(path + newFileName));
+                System.out.println(path + newFileName);
+                resMsg = new ResMsg<>(1, "Upload Success",newFileName);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return resMsg;
+    }
 
 }
